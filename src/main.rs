eea0db050b62c4e2e1f9b074e61dd4ecaf748cc4
Peter Holloway <holloway.p.r@gmail.com>
2025-05-12 16:25:26 +0100

    Replace ureq with reqwest and make client async
    When handling messages from a messagebus alongside the http requests, it
    will be necessary to make requests non-blocking.

--- src/main.rs
+++ src/main.rs
@@ -3,87 +3,85 @@ use std::collections::HashMap;
 use clap::Parser;
 use cli::{CliArgs, RunArgs};
 use entities::{Device, DeviceList, PlanList, TaskReference};
+use reqwest::Url;
 use serde::de::DeserializeOwned;
 use serde_json::Value;
-use ureq::Agent;
-use ureq::http::{Uri, uri};
+use tokio::runtime::Runtime;
 
 mod cli;
 mod entities;
 
 fn main() {
     let args = CliArgs::parse();
-    let client: Agent = Agent::config_builder()
-        .http_status_as_error(false)
-        .build()
-        .into();
+
+    let client = reqwest::Client::new();
     let client = Client {
         agent: client,
-        host: Uri::from_static("http://localhost:8000"),
+        host: Url::parse("http://localhost:8000").unwrap(),
     };
-    match args {
-        CliArgs::Run(run_args) => client.run_plan(run_args),
-        CliArgs::Devices { name: filter } => client.get_devices(filter),
-        CliArgs::Plans { name } => client.get_plans(name),
-    }
+
+    let rt = Runtime::new().expect("Couldn't create runtime");
+    rt.block_on(async {
+        match args {
+            CliArgs::Run(run_args) => client.run_plan(run_args).await,
+            CliArgs::Devices { name: filter } => client.get_devices(filter).await,
+            CliArgs::Plans { name } => client.get_plans(name).await,
+        }
+    });
 }
 
 struct Client {
-    agent: Agent,
-    host: Uri,
+    agent: reqwest::Client,
+    host: Url,
 }
 
 impl Client {
-    fn request_path(&self, path: &str) -> Uri {
-        uri::Builder::from(self.host.clone())
-            .path_and_query(path)
-            .build()
-            .unwrap()
-    }
-
-    fn run_plan(&self, args: RunArgs) {
+    async fn run_plan(&self, args: RunArgs) {
         let req = self
             .agent
-            .post(self.request_path("/tasks"))
-            .send_json(HashMap::from([
+            .post(self.endpoint("/tasks"))
+            .json(&HashMap::from([
                 ("name".to_owned(), Value::String(args.name().into())),
                 ("params".to_owned(), args.parameters().unwrap().unwrap()),
-            ]));
-        let task = req
-            .unwrap()
-            .body_mut()
-            .read_json::<TaskReference>()
-            .unwrap();
-        let mut resp = self
+            ]))
+            .send()
+            .await;
+        let task = req.unwrap().json::<TaskReference>().await.unwrap();
+        let resp = self
             .agent
-            .put(self.request_path("/worker/task"))
-            .send_json(task)
+            .put(self.endpoint("/worker/task"))
+            .json(&task)
+            .send()
+            .await
             .unwrap();
         println!("{resp:#?}");
-        println!("{:?}", resp.body_mut().read_to_string());
+        println!("{:?}", resp.text().await)
     }
 
-    fn get_devices(&self, name: Option<String>) {
+    async fn get_devices(&self, name: Option<String>) {
         match name {
             Some(name) => println!(
                 "{:#?}",
-                self.get::<Device>(self.request_path(&format!("/devices/{name}")))
+                self.get::<Device>(self.endpoint(&format!("/devices/{name}")))
+                    .await
             ),
             None => println!(
                 "{:#?}",
-                self.get::<DeviceList>(self.request_path("/devices"))
+                self.get::<DeviceList>(self.endpoint("/devices")).await
             ),
         }
     }
 
-    fn get_plans(&self, name: Option<String>) {
+    async fn get_plans(&self, name: Option<String>) {
         let plans = match name {
             Some(name) => vec![
-                self.get(self.request_path(&format!("/plans/{name}")))
+                self.get(self.endpoint(&format!("/plans/{name}")))
+                    .await
                     .unwrap(),
             ],
             None => {
-                self.get::<PlanList>(self.request_path("/plans"))
+                self.get::<PlanList>(self.endpoint("/plans"))
+                    .await
                     .unwrap()
                     .plans
             }
@@ -93,7 +91,12 @@ impl Client {
             println!("{}", plan.description.as_deref().unwrap_or("???"));
         }
     }
-    fn get<T: DeserializeOwned>(&self, url: Uri) -> Result<T, ureq::Error> {
-        self.agent.get(url).call().unwrap().body_mut().read_json()
+
+    async fn get<T: DeserializeOwned>(&self, url: Url) -> Result<T, reqwest::Error> {
+        self.agent.get(url).send().await.unwrap().json().await
+    }
+
+    fn endpoint(&self, path: &str) -> Url {
+        self.host.join(path).unwrap()
     }
 }
