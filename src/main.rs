089f710e674850edb6a8aaa004396ee9e22f65e1
Peter Holloway <holloway.p.r@gmail.com>
2025-05-12 15:30:20 +0100

    Create central client type instead of disparate functions

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,99 @@
+use std::collections::HashMap;
+
 use clap::Parser;
 use cli::{CliArgs, RunArgs};
 use entities::{Device, DeviceList, PlanList, TaskReference};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
-use ureq::SendBody;
+use ureq::Agent;
+use ureq::http::{Uri, uri};
 
 mod cli;
 mod entities;
 
 fn main() {
     let args = CliArgs::parse();
+    let client: Agent = Agent::config_builder()
+        .http_status_as_error(false)
+        .build()
+        .into();
+    let client = Client {
+        agent: client,
+        host: Uri::from_static("http://localhost:8000"),
+    };
     match args {
-        CliArgs::Run(run_args) => run_plan(run_args),
-        CliArgs::Devices { name: filter } => get_devices(filter),
-        CliArgs::Plans { name } => get_plans(name),
+        CliArgs::Run(run_args) => client.run_plan(run_args),
+        CliArgs::Devices { name: filter } => client.get_devices(filter),
+        CliArgs::Plans { name } => client.get_plans(name),
     }
 }
 
-fn run_plan(args: RunArgs) {
-    let req = ureq::post("http://localhost:8000/tasks").send(
-        SendBody::from_json(&Value::Object(
-            [
+struct Client {
+    agent: Agent,
+    host: Uri,
+}
+
+impl Client {
+    fn request_path(&self, path: &str) -> Uri {
+        uri::Builder::from(self.host.clone())
+            .path_and_query(path)
+            .build()
+            .unwrap()
+    }
+
+    fn run_plan(&self, args: RunArgs) {
+        let req = self
+            .agent
+            .post(self.request_path("/tasks"))
+            .send_json(HashMap::from([
                 ("name".to_owned(), Value::String(args.name().into())),
                 ("params".to_owned(), args.parameters().unwrap().unwrap()),
-            ]
-            .into_iter()
-            .collect(),
-        ))
-        .unwrap(),
-    );
-    let task = req
-        .unwrap()
-        .body_mut()
-        .read_json::<TaskReference>()
-        .unwrap();
-    let mut resp = ureq::put("http://localhost:8000/worker/task")
-        .config()
-        .http_status_as_error(false)
-        .build()
-        .send_json(task)
-        .unwrap();
-    println!("{resp:#?}");
-    println!("{:?}", resp.body_mut().read_to_string());
-}
-
-fn get_devices(name: Option<String>) {
-    match name {
-        Some(name) => println!(
-            "{:#?}",
-            get::<Device>(format!("http://localhost:8000/devices/{name}"))
-        ),
-        None => println!("{:#?}", get::<DeviceList>("http://localhost:8000/devices")),
+            ]));
+        let task = req
+            .unwrap()
+            .body_mut()
+            .read_json::<TaskReference>()
+            .unwrap();
+        let mut resp = self
+            .agent
+            .put(self.request_path("/worker/task"))
+            .send_json(task)
+            .unwrap();
+        println!("{resp:#?}");
+        println!("{:?}", resp.body_mut().read_to_string());
     }
-}
 
-fn get_plans(name: Option<String>) {
-    let plans = match name {
-        Some(name) => vec![get(format!("http://localhost:8000/plans/{name}")).unwrap()],
-        None => {
-            get::<PlanList>("http://localhost:8000/plans")
-                .unwrap()
-                .plans
+    fn get_devices(&self, name: Option<String>) {
+        match name {
+            Some(name) => println!(
+                "{:#?}",
+                self.get::<Device>(self.request_path(&format!("/devices/{name}")))
+            ),
+            None => println!(
+                "{:#?}",
+                self.get::<DeviceList>(self.request_path("/devices"))
+            ),
         }
-    };
-    for plan in plans {
-        println!("{}", plan.name,);
-        println!("{}", plan.description.as_deref().unwrap_or("???"));
     }
-}
 
-fn get<T: DeserializeOwned>(url: impl Into<String>) -> Result<T, ureq::Error> {
-    ureq::get(url.into())
-        .config()
-        .http_status_as_error(false)
-        .build()
-        .call()
-        .unwrap()
-        .body_mut()
-        .read_json()
+    fn get_plans(&self, name: Option<String>) {
+        let plans = match name {
+            Some(name) => vec![
+                self.get(self.request_path(&format!("/plans/{name}")))
+                    .unwrap(),
+            ],
+            None => {
+                self.get::<PlanList>(self.request_path("/plans"))
+                    .unwrap()
+                    .plans
+            }
+        };
+        for plan in plans {
+            println!("{}", plan.name,);
+            println!("{}", plan.description.as_deref().unwrap_or("???"));
+        }
+    }
+    fn get<T: DeserializeOwned>(&self, url: Uri) -> Result<T, ureq::Error> {
+        self.agent.get(url).call().unwrap().body_mut().read_json()
+    }
 }
